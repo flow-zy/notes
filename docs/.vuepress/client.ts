0fd7f8730196c1bf0b7afca7e99e2b3887b8a561
flow-zy <84438296+flow-zy@users.noreply.github.com>
2023-07-31 20:04:24 +0800

    fix: document is defined

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,35 +1,37 @@
-import { defineClientConfig } from '@vuepress/client'
+import { defineClientConfig } from '@vuepress/client';
 import {
   Popper,
   PopperShape,
   type PopperConfig,
   MAX_Z_INDEX,
   type CanvasOptions,
-} from '@moefy-canvas/theme-popper'
-import { onMounted } from 'vue'
+} from '@moefy-canvas/theme-popper';
+import { onMounted, onBeforeMount } from 'vue';
 const themeConfig: PopperConfig = {
   shape: PopperShape.Star,
   size: 1.75,
   numParticles: 10,
-}
+};
 
 const canvasOptions: CanvasOptions = {
   opacity: 1,
   zIndex: MAX_Z_INDEX,
-}
+};
 
 export default defineClientConfig({
   enhance({ app, router, siteData }) {},
   setup() {
-    const canvas = document && document.createElement('canvas')
-    canvas.id = 'moefy-canvas'
-    const doc = document.querySelector('#app') as HTMLDivElement
-    doc.appendChild(canvas)
+    onBeforeMount(() => {
+      const canvas = document && document.createElement('canvas');
+      canvas.id = 'moefy-canvas';
+      const doc = document.querySelector('#app') as HTMLDivElement;
+      doc.appendChild(canvas);
+    });
     onMounted(() => {
-      const el = document.getElementById('moefy-canvas')
-      const popper = new Popper(themeConfig, canvasOptions)
-      popper.mount(el as HTMLCanvasElement)
-    })
+      const el = document.getElementById('moefy-canvas');
+      const popper = new Popper(themeConfig, canvasOptions);
+      popper.mount(el as HTMLCanvasElement);
+    });
   },
   rootComponents: [],
-})
+});
